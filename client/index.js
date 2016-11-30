7562373f113ecc190291c390e446b237d662996f
Juan Carlos <juancafe2@gmail.com>
2016-11-30 15:54:01 -0800

    Fixing a bug when server is trying to read 'document'

--- client/index.js
+++ client/index.js
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 // Import the main component
 import App from './Components/App';
 // Renders the App component into the root of our main html file: index.html
-if (typeof window !== 'undefined') {
+if (typeof document !== 'undefined') {
   ReactDOM.render(<App />, document.getElementById('root'));
 }
